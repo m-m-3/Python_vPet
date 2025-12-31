3842aadfbce2de4b22ad1be90a81663a28304966
m-m-3 <marcin.migula@student.wsb.edu.pl>
2025-12-31 13:50:53 +0000

    Dodanie menu i klas

---
+++ pet.py
@@ -0,0 +1,22 @@
+from status import Status
+
+class Pet:
+    def __init__(self, Name=""):
+        self.Name = Name
+        self.Status = Status()
+
+    def Feed(self, chosenFood):
+        # TODO
+        pass
+
+    def Play(self, chosenToy):
+        # TODO
+        pass
+
+    def SpendTime(self, choice):
+        # TODO
+        pass
+
+    def IsGameOver(self):
+        # TODO
+        return False
--- program.py
+++ program.py
@@ -1,6 +1,46 @@
-class Program:
-    def main():
-        print("Hello, world!")
+import os
+from pet import Pet
+
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def pause():
+    input("Wciśnij Enter, aby wrócić do menu...")
+
+def main():
+    clear()
+    print("===== WIRTUALNE ZWIERZĄTKO =====")
+    pet_name = input("Podaj imię zwierzątka: ") or ""
+    pet = Pet(Name=pet_name)
+
+    while True:
+        clear()
+        print(f"===== {pet.Name} =====")
+        print("1. Statystyki")
+        print("2. Nakarm")
+        print("3. Pobaw się")
+        print("4. Spędź czas")
+        print("0. Wyjście")
+
+        choice = input("Wybierz opcję: ")
+
+        if choice == "1":
+            print("TODO: Statystyki")
+            pause()
+        elif choice == "2":
+            print("TODO: Karmienie")
+            pause()
+        elif choice == "3":
+            print("TODO: Zabawa")
+            pause()
+        elif choice == "4":
+            print("TODO: Spędź czas")
+            pause()
+        elif choice == "0":
+            return
+        else:
+            print("Niepoprawny wybór.")
+            pause()
 
 if __name__ == "__main__":
-    Program.main()
+    main()
